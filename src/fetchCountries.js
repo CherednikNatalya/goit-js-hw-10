b51cf3ad23f1c8d931e4976e18ae2e53e8c9bec3
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-20 22:11:40 +0200

    12

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+// export function fetchCountries(name) {
+    
+// }
+
+// console.log(fetchCountries);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+
+// import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      createMarkup(data);
+    })
+    .catch(error => {
+        console.log(error.message);
+    });
+
+// function createMarkup({name, capital, population, flags, languages}) {
+// console.log(name.official);
+// console.log(capital[0]);
+// console.log(population);
+// console.log(flags.svg);
+// console.log(languages[0].name);
+//   }
+
+ 
