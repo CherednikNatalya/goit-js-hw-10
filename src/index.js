76541018fce109fc0cce2422a0cdc99114024e1b
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-11-20 22:50:51 +0200

    jxrt

--- src/index.js
+++ src/index.js
@@ -3,29 +3,38 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
 // import { fetchCountries } from './fetchCountries';
-
+// let countriesName = null
 const DEBOUNCE_DELAY = 300;
 
-    fetch('https://restcountries.com/v3.1/all')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      createMarkup(data);
-    })
-    .catch(error => {
-        console.log(error.message);
-    });
 
-// function createMarkup({name, capital, population, flags, languages}) {
-// console.log(name.official);
-// console.log(capital[0]);
-// console.log(population);
-// console.log(flags.svg);
-// console.log(languages[0].name);
+    function fetchCountries(name) {
+      return fetch(
+        `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+      )
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+          return response.json();
+        })
+    
+        .catch(err => console.log('Error!'));
+    }
+
+
+    
+// function createMarkup({name}) {
+// console.log(name.common);
 //   }
+
+
+  
+  // function createMarkup({name, capital, population, flags, languages}) {
+  //   console.log(name.official);
+  //   console.log(capital[0]);
+  //   console.log(population);
+  //   console.log(flags.svg);
+  //   console.log(languages[0].name);
+  //     }
 
  
